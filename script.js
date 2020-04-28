a1cb83b2a16c3724a60089bcacdc6432ac441f03
usman-203 <62180188+usman-203@users.noreply.github.com>
2020-04-27 17:18:46 -0700

    commit7

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const currencyEl_two = document.getElementById("currency-two");
 const amountEl_two = document.getElementById("amount-two");
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
+
 function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
